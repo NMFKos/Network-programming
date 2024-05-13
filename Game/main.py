d7b11675d8aae7451d41d4c81b3bb892ac8cebe3
Dương Đạt <144459605+NMFKos@users.noreply.github.com>
2024-05-13 09:33:21 +0700

    Add files via upload

--- Game/main.py
+++ Game/main.py
@@ -2,6 +2,7 @@ import customtkinter as ctk
 from customtkinter import *
 from PIL import Image, ImageTk
 import subprocess
+import pygame
 
 # Thiết lập cửa sổ chính
 app = CTk()
@@ -18,7 +19,23 @@ def pve_mode():
     app.destroy()
     subprocess.run(['python', 'PongCPU.py'])
 
+def return_home():
+    btn_play.place(x=370, y=300)
+    tab_personal.place(x=10, y=20)
+    tab_notifications.place(x=550, y=20)
+    tab_sound.place(x=720, y=20)
+
+    btn_online.place_forget()
+    btn_machine.place_forget()
+    btn_offline.place_forget()
+    return_label.place_forget()
+
 def mode_games():
+    global btn_machine
+    global btn_offline
+    global btn_online
+    global return_label
+
     btn_machine = CTkButton(app, text='MÁY',command=pve_mode, width=120, height=50, corner_radius=5, cursor="hand2")
     btn_machine.place(x=220, y=200)
 
@@ -32,7 +49,12 @@ def mode_games():
     tab_notifications.place_forget()
     tab_sound.place_forget()
     tab_personal.place_forget()
+
+    return_label = CTkButton(app, text='Trở về',command=return_home, width=120, height=30)
+    return_label.place(x=10, y=20)
     
+def sound():
+    subprocess.run(["python3", "./Volume/Volume/Volume/volume.py"])
 
 
 bg_img = CTkImage(dark_image=Image.open("bgmain.jpg"), size=(850, 500))
@@ -51,7 +73,10 @@ tab_personal.place(x=10, y=20)
 tab_notifications = CTkButton(app, text='Thông báo', width=120, height=30)
 tab_notifications.place(x=550, y=20)
 
-tab_sound = CTkButton(app, text='Âm thanh', width=120, height=30)
+tab_sound = CTkButton(app, text='Âm thanh',command=sound, width=120, height=30)
 tab_sound.place(x=720, y=20)
+pygame.mixer.init()
+pygame.mixer.music.load('sounds/background_music.mp3')
+pygame.mixer.music.play(-1)
 
 app.mainloop()
